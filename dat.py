ed44e14d813644bc5e89d51d52dddeb577d8f72f
Matthew Ratliff <matthewratliff180@gmail.com>
2023-07-27 20:06:50 -0500

    My first github project

---
+++ dat.py
@@ -0,0 +1,34 @@
+
+y = "John"
+
+
+print(y)
+
+cars = {"Ford", "Volvo", "BMW"}
+cars.append(cars)
+for x in cars:
+    print(x)
+def my_name():
+print("hello")
+
+my_name()
+# print(array)
+# for array in cars:
+#     print(array)
+
+# first_number = int(input('Type the first number:'))  ;\
+#     second_number = int(input('Type the second number: ')) ;\
+#         print("The sum is: ", first_number + second_number)
+# name = input('Enter your name:')
+# print (type(name))
+# x = 5 
+# print ('The number is'+ str(x))
+# name = "Matt"
+# print(name)
+
+# print (x,y)                                                                                                                                                                                  b 
+
+# print(type(name))
+
+# timetuple = ("apple", "cherry", "hawkeye")
+
